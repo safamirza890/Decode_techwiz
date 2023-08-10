64be90d263f85f9c81d4a45c472d7cbd80816738
fareedmunawar <fareed220901e@aptechgdn.net>
2023-08-10 15:24:32 +0500

    updated

--- resources/views/accesories.blade.php
+++ resources/views/accesories.blade.php
@@ -10,8 +10,8 @@
   <div class="product-card">
     <img class="product-image" src="assests/images/person_2.jpg" alt="Product Image">
     <h2 class="product-title">Product Name</h2>
-    <p class="product-price">$XX.XX</p>
-    <button class="add-to-cart-button" onclick="openModal()">Add to Cart</button>
+    <p class="product-price">$229</p>
+    <button class="add-to-cart-button" onclick="openModal(229)">Add to Cart</button>
     <button class="wishlist-button"><span class="heart-icon">❤️</span> Add to Wishlist</button>
   </div>
 </div>
@@ -22,20 +22,27 @@
     <span class="close" onclick="closeModal()">&times;</span>
     <img class="modal-product-image" src="assests/images/person_2.jpg" alt="Product Image">
     <p class="modal-product-title">Product Name</p>
+    <p class="modal-product-price">$XX.XX</p> <!-- Placeholder for the price -->
     <div class="quantity-container">
       <label for="quantity">Quantity:</label>
-      <input type="number" id="quantity" value="1" min="1" onchange="updateTotalPrice()">
+      <input type="number" id="quantity" value="1" min="1" onchange="updateTotalPrice(229)">
     </div>
     <p class="total-price">Total Price: $XX.XX</p>
     <button class="buy-now-button" onclick="buyNow()">Buy Now</button>
   </div>
 </div>
 
-
 <script>
-  function openModal() {
+  function openModal(price) {
     var modal = document.getElementById('cartModal');
     modal.style.display = 'block';
+
+    // Set the price in the modal
+    document.querySelector('.modal-product-price').textContent = "$" + price.toFixed(2);
+
+    // Reset quantity and total price when modal is opened
+    document.getElementById('quantity').value = 1;
+    updateTotalPrice(price);
   }
 
   function closeModal() {
@@ -43,19 +50,19 @@
     modal.style.display = 'none';
   }
 
-  function updateTotalPrice() {
+  function updateTotalPrice(price) {
     var quantity = parseInt(document.getElementById('quantity').value);
-    var price = parseFloat("$XX.XX"); // Replace with actual price
     var total = quantity * price;
     document.querySelector('.total-price').textContent = "Total Price: $" + total.toFixed(2);
   }
+
   function buyNow() {
     var quantity = parseInt(document.getElementById('quantity').value);
-    var price = parseFloat("$XX.XX"); // Replace with actual price
+    var price = parseFloat("229"); // Use the actual price from the card
     var total = quantity * price;
-    
+
     // Perform the actual "Buy Now" action here, such as redirecting to a checkout page or performing payment processing
-    
+
     closeModal(); // Close the modal after performing the action
   }
 </script>
